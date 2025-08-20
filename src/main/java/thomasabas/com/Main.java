cb637287d8c91ebb946f18563ba7c072fdee8be8
Thomas Abas <abasienyenethomas2@gmail.com>
2025-08-20 08:27:59 +0100

    No 3 done

--- src/main/java/thomasabas/com/Main.java
+++ src/main/java/thomasabas/com/Main.java
@@ -1,5 +1,7 @@
 package thomasabas.com;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -20,18 +22,30 @@ public class Main {
 //        System.out.println("Highest discovery: " + maxValue + " barrels at position " + MaxIndex);
 //    }
 
+//    public static void main(String[] args) {
+//        // (CoastalRefine Ltd
+//        int[] litres = {200, 800, 1500, 2000, 1750, 1400, 1650};
+//        int total = 0;
+//        for (int i = 0; i < litres.length; i++) {
+//            total += litres[i];
+//
+//        }
+//        if (total > 1000){
+//            System.out.println("ALERT: Capacity Exceeded!");
+//        }
+//    }
+
+
     public static void main(String[] args) {
-        // (CoastalRefine Ltd
-        int[] litres = {200, 800, 1500, 2000, 1750, 1400, 1650};
-        int total = 0;
-        for (int i = 0; i < litres.length; i++) {
-            total += litres[i];
-
+//        Defective parts report
+        int[] defects = {2, 8, 1, 0, 6, 7, 3, 9, 5};
+        ArrayList<Integer> filtered = new ArrayList<>();
+        for (int i = 0; i < defects.length; i++) {
+            if (defects[i] > 5) {
+                filtered.add(defects[i]);
+            }
         }
-        if (total > 1000){
-            System.out.println("ALERT: Capacity Exceeded!");
-        }
+        System.out.println(filtered);
     }
-
 
 }
